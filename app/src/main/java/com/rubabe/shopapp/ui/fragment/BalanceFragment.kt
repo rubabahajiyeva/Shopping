b5907a75753d0cba8a903edbbff8d4cff1093a49
Rubabe <rubabahajieva@gmail.com>
2023-11-19 15:50:39 +0400

    Changed App color

--- app/src/main/java/com/rubabe/shopapp/ui/fragment/BalanceFragment.kt
+++ app/src/main/java/com/rubabe/shopapp/ui/fragment/BalanceFragment.kt
@@ -1,26 +1,42 @@
 package com.rubabe.shopapp.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.rubabe.shopapp.R
+import com.rubabe.shopapp.databinding.FragmentBalanceBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BalanceFragment : Fragment() {
 
+    private var _binding: FragmentBalanceBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_balance, container, false)
+    ): View {
+        _binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_balance, container, false)
+
+        binding.profileToolbarHeader = "Balance"
+
+        binding.back.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_balanceFragment_to_profileFragment)
+        }
+        return binding.root
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
